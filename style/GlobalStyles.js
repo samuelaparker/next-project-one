c492e9736a98ece5ea029df22e3dbb51be0d1164
samuelaparker <samuel.a.parker00@gmail.com>
2021-11-09 17:42:32 -0500

    globalstyles

---
+++ style/GlobalStyles.js
@@ -0,0 +1,82 @@
+import { createGlobalStyle } from "styled-components";
+export const GlobalStyles = createGlobalStyle`
+
+
+  *, *::before, *::after {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    font-family: program, sans-serif;
+
+font-weight: 900;
+
+font-style: normal;
+
+    
+  }
+  body {
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+    font-family: program, sans-serif;
+    font-weight: 900;
+    font-style: normal;
+    font-size: 16px;
+  
+    
+  }
+  html {
+    margin: 0;  
+  }
+
+  div {
+    font-weight: 400;
+    font-family: program, sans-serif;
+
+font-weight: 900;
+
+font-style: normal;
+   
+    
+  }
+
+  h1 {
+    font-family: program, sans-serif;
+
+font-weight: 900;
+
+font-style: normal;
+  }
+  h2 {
+    
+    font-family: program, sans-serif;
+
+font-weight: 900;
+
+font-style: normal;
+    font-size: 16px;
+    font-weight: 400;
+    margin: 5px 0;
+  }
+  p {
+    font-family: program, sans-serif;
+
+font-weight: 900;
+
+font-style: normal;
+
+    font-size: 12px;
+    font-weight: 400;
+    margin: 5px 0;
+  }
+
+  a {
+    text-decoration: none;
+    font-family: program, sans-serif;
+
+font-weight: 900;
+
+font-style: normal;
+    
+  }
+`;
