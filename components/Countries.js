af06c9167fb3f775aa48246e245d906bde296cd4
samuelaparker <samuel.a.parker00@gmail.com>
2021-11-10 18:58:19 -0500

    experiment in data fetching with apollo -wip

---
+++ components/Countries.js
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+import { gql } from "@apollo/client";
+import client from "../apollo-client";
+
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: gql`
+      query Countries {
+        countries {
+          code
+          name
+          emoji
+        }
+      }
+    `,
+  })
+
+  return {
+    props: {
+      countries: data.countries.slice(0, 4),
+    },
+  };
+}
+
+const Countries = ({countries}) => {
+
+    console.log(countries)
+  return (
+    <>
+      <div>
+        {countries.map((n) => (
+          <ListItem key={n.code}>
+            <p>{n.name}</p>
+          </ListItem>
+        ))}
+      </div>
+    </>
+  );
+};
+
+
+export default Countries;
+
+
+const ListItem = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 10px;
+  padding: 10px;
+  border: 1px solid black;
+  border-radius: 5px;
+`;
--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,50 @@ import Head from "next/head";
 import Image from "next/image";
 import styled from "styled-components";
 import MomAndDad from "../public/images/_15_00841.jpg";
+import Countries from "../components/Countries"
 
-export default function Home() {
+
+export async function getServerSideProps(context) {
+  const res = await fetch(`https://cat-fact.herokuapp.com/facts`)
+  const data = await res.json()
+
+  if (!data) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: { data }, // will be passed to the page component as props
+  }
+}
+
+
+
+export default function Home({ data }) {
+  console.log(data);
   return (
     <>
       <Head>
         <title>Next Project One</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <Wrapper>
-      <Title>Home Page</Title>
-      <ImageWrap>
-        <Image src={MomAndDad} alt="Mom and Dad"  />
-      </ImageWrap>
+        <Title>Home Page</Title>
+        <ImageWrap>
+          <Image src={MomAndDad} alt="Mom and Dad" />
+        </ImageWrap>
       </Wrapper>
+      <div>
+        {data.map((n) => (
+          <ListItem key={n._id}>
+            <p>{n.text}</p>
+            <p>{n.emoji}</p>
+            <p>{n.code}</p>
+          </ListItem>
+        ))}
+      </div>
+      <Countries />
     </>
   );
 }
@@ -26,8 +55,7 @@ const Wrapper = styled.div`
   flex-direction: row;
 
   justify-content: center;
-  height: 100vh;
-  `
+`;
 
 const Title = styled.h1`
   position: absolute;
@@ -40,5 +68,14 @@ const Title = styled.h1`
 
 const ImageWrap = styled.div`
   max-width: 50%;
-  height: 100%;
+`;
+
+const ListItem = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 10px;
+  padding: 10px;
+  border: 1px solid black;
+  border-radius: 5px;
 `;
