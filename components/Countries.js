158d7db9cd17e67e48c1cb440652fc03a1e90205
samuelaparker <samuel.a.parker00@gmail.com>
2021-11-10 22:26:19 -0500

    apollo tests cont

--- components/Countries.js
+++ components/Countries.js
@@ -1,53 +1,54 @@
-import styled from "styled-components";
-import { gql } from "@apollo/client";
-import client from "../apollo-client";
+// import styled from "styled-components";
+// import { gql } from "@apollo/client";
+// import client from "../apollo-client";
 
-export async function getStaticProps() {
-  const { data } = await client.query({
-    query: gql`
-      query Countries {
-        countries {
-          code
-          name
-          emoji
-        }
-      }
-    `,
-  })
+// export async function getStaticProps(context) {
+//   const { data } = await client.query({
+//     query: gql`
+//       query Countries {
+//         countries {
+//           code
+//           name
+//           emoji
+//         }
+//       }
+//     `,
+//   })
 
-  return {
-    props: {
-      countries: data.countries.slice(0, 4),
-    },
-  };
-}
+//   return {
+//     props: {
+//       countries: data.countries.slice(0, 4),
+//     },
+//   };
+// }
 
-const Countries = ({countries}) => {
+// const Countries = (props) => {
 
-    console.log(countries)
-  return (
-    <>
-      <div>
-        {countries.map((n) => (
-          <ListItem key={n.code}>
-            <p>{n.name}</p>
-          </ListItem>
-        ))}
-      </div>
-    </>
-  );
-};
-
-
-export default Countries;
+//     console.log(props)
+//   return (
+//     <>
+//       <div>
+//         {/* {countries.map((n) => (
+//           <ListItem key={n.code}>
+//             <p>{n.name}</p>
+//           </ListItem>
+//         ))} */}
+        
+//       </div>
+//     </>
+//   );
+// };
 
 
-const ListItem = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  margin: 10px;
-  padding: 10px;
-  border: 1px solid black;
-  border-radius: 5px;
-`;
+// export default Countries;
+
+
+// const ListItem = styled.div`
+//   display: flex;
+//   flex-direction: row;
+//   justify-content: space-between;
+//   margin: 10px;
+//   padding: 10px;
+//   border: 1px solid black;
+//   border-radius: 5px;
+// `;
--- pages/index.js
+++ pages/index.js
@@ -2,28 +2,49 @@ import Head from "next/head";
 import Image from "next/image";
 import styled from "styled-components";
 import MomAndDad from "../public/images/_15_00841.jpg";
-import Countries from "../components/Countries"
+// import Countries from "../components/Countries"
+import { gql } from "@apollo/client";
+import client from "../apollo-client";
 
 
-export async function getServerSideProps(context) {
-  const res = await fetch(`https://cat-fact.herokuapp.com/facts`)
-  const data = await res.json()
+// export async function getServerSideProps(context) {
+//   const res = await fetch(`https://cat-fact.herokuapp.com/facts`)
+//   const data = await res.json()
 
-  if (!data) {
-    return {
-      notFound: true,
-    }
-  }
+//   if (!data) {
+//     return {
+//       notFound: true,
+//     }
+//   }
+
+//   return {
+//     props: { data }, // will be passed to the page component as props
+//   }
+// }
+
+export async function getStaticProps(context) {
+  const { data } = await client.query({
+    query: gql`
+      query Countries {
+        countries {
+          code
+          name
+          emoji
+        }
+      }
+    `,
+  })
 
   return {
-    props: { data }, // will be passed to the page component as props
-  }
+    props: {
+      countries: data.countries.slice(0, 4),
+    },
+  };
 }
 
 
-
-export default function Home({ data }) {
-  console.log(data);
+export default function Home({countries}) {
+  console.log(countries)
   return (
     <>
       <Head>
@@ -36,7 +57,7 @@ export default function Home({ data }) {
           <Image src={MomAndDad} alt="Mom and Dad" />
         </ImageWrap>
       </Wrapper>
-      <div>
+      {/* <div>
         {data.map((n) => (
           <ListItem key={n._id}>
             <p>{n.text}</p>
@@ -44,8 +65,8 @@ export default function Home({ data }) {
             <p>{n.code}</p>
           </ListItem>
         ))}
-      </div>
-      <Countries />
+      </div> */}
+      {/* <Countries /> */}
     </>
   );
 }
